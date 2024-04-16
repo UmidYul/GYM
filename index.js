c89459a39ae9d66ca09859003b8c1ca44d18bd7f
UmidYul <yumid257@gmail.com>
2024-04-16 15:48:10 +0500

    цуйу

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ import { SendEmail } from "./email.js"
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
-const file = join(__dirname, '/tmp/','db.json')
+const file = join(__dirname, 'db.json')
 const adapter = new JSONFile(file)
 const defaultData = { admin: [], members: [], plans: [], coaches: [] }
 const db = new Low(adapter, defaultData)
@@ -19,7 +19,7 @@ app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 app.use(express.static(__dirname + "/public/"))
 app.use((err, req, res, next) => {
-    if(err){
+    if (err) {
         res.send(err)
     }
 })
